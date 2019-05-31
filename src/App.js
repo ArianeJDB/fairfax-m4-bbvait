90391ca6e670affb1123f183f8aa20dd1e5806f0
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-05-31 16:39:34 +0200

    default route functioning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import Areas from './components/Areas';
 import './App.css';
 
 class App extends React.Component {
   render () {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route
+            path="/:areasID"
+            render={props => <Areas match={props.match}/>}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;

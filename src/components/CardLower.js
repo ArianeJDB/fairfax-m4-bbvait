c729cc9e84ba5b53797f5f8369d9c77daffc387f
LorenaBH <lorenabenitoherrero@gmail.com>
2019-06-03 19:11:13 +0200

    doing styles again

--- src/components/CardLower.js
+++ src/components/CardLower.js
@@ -21,12 +21,12 @@ function CardLower (props) {
       </div>
       <div className="block3">
         <div className="card__details">
-          <h3 className="card__email">{`mailto:${email}`}</h3>
-          <h3 className="card__phone">{phone}</h3>
-          <h3 className="card__area">{area}</h3>
+          <p className="card__email">{email}</p>
+          <p className="card__phone">{phone}</p>
         </div>
         <div className="card__link">
-          <Link to="/areas1">Detalles</Link>
+        <h3 className="card__area">{area}</h3>
+          <Link className="link" to="/areas1">Detalles</Link>
         </div>
       </div>
     </div>

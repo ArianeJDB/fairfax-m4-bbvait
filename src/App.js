377be65fc0fb71e83cd8479da14284e1ab8c88b4
E053689 <nathan.adam@bbvaitspain.com>
2019-06-03 20:59:03 +0200

    example: refactors App class component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {fetchPeople} from './data/Data';
 import {Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
@@ -6,75 +6,62 @@ import Areas from './components/Areas';
 import Managers from './components/Managers';
 // import './App.css';
 
-class App extends React.Component {
-  constructor (props) {
-    super (props);
-    this.state = {
-      chief: [],
-      executives: [],
-      managers: [],
-      projects: [],
-      staff: [],
-      isLoading: true,
-    };
-  }
+const App = props => {
+  const [state, setState] = useState({
+    chief: [],
+    executives: [],
+    managers: [],
+    projects: [],
+    staff: [],
+    fetching: true,
+  });
+  
 
-  componentDidMount () {
-    this.FetchAllData ();
-  }
-
-  FetchAllData () {
-    fetchPeople ().then (people => {
-      this.setState ({
+  useEffect(() => {
+    fetchPeople().then (people => {
+      setState({
         chief: [...people['chief executive']],
         executives: [...people.executives],
         managers: [...people.managers],
         projects: [...people.projects],
         staff: [...people.staff],
-        isLoading: false,
+        fetching: false,
       });
     });
-  }
-  render () {
-    const {
-      chief,
-      executives,
-      managers,
-      projects,
-      staff,
-      isLoading,
-    } = this.state;
+  }, []);
 
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
-    return (
-      <div className="App">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <Home
-                chief={chief}
-                executives={executives}
-              />
-            )}
-          />
-          <Route
-            path="/areas/:id"
-            render={props => (
-              <Areas
-                match={props.match}
-                executives={executives}
-                managers={managers}
-              />
-            )}
-          />
-          <Route path="/managers/:id" component={Managers} />
-        </Switch>
-      </div>
-    );
+
+  if (state.fetching) {
+    return <div>Loading...</div>;
   }
-}
+
+  return (
+    <div className="App">
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Home
+              chief={state.chief}
+              executives={state.executives}
+            />
+          )}
+        />
+        <Route
+          path="/areas/:id"
+          render={props => (
+            <Areas
+              match={props.match}
+              executives={state.executives}
+              managers={state.managers}
+            />
+          )}
+        />
+        <Route path="/managers/:id" component={Managers} />
+      </Switch>
+    </div>
+  );
+};
+
 export default App;
